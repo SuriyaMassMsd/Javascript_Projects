57aa1aa6a2bd4377095831a55d03db77bb9c0a48
Ajisurimass@7 <ksmass2001@gmail.com>
2023-12-29 20:52:20 +0530

    deploy:adding more html files

--- Javascript-Dom/02.pizza/vite.config.js
+++ Javascript-Dom/02.pizza/vite.config.js
@@ -1,5 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path'; // Import resolve from path module
 
-export default defineConfig ({
-    base:"/cyberdude-challenges/"
-})
+export default defineConfig({
+  base: '/cyberdude-challenges/',
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        update: resolve(__dirname, 'update.html'),
+        success: resolve(__dirname, 'success.html'),
+        mainjs: resolve(__dirname, './assets/js/main.js'),
+        canceljs: resolve(__dirname, './assets/js/cancel.js'),
+        updatejs: resolve(__dirname, './assets/js/update.js'),
+        customcss : resolve(__dirname , '/assets/css/custom.css')
+      },
+    },
+  },
+});
