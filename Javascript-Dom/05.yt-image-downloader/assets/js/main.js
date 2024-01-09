eb806111454413ec6ce538acb7162d0f5c4f0869
Ajisurimass@7 <ksmass2001@gmail.com>
2024-01-09 10:21:06 +0530

    added : yt playlist thumbnail

--- Javascript-Dom/05.yt-image-downloader/assets/js/main.js
+++ Javascript-Dom/05.yt-image-downloader/assets/js/main.js
@@ -13,16 +13,57 @@ formEl.addEventListener("submit", function (e) {
 function hdiamge() {
   const inputEl = document.querySelector("#input").value;
 
-  show.classList.remove("hidden");
+
+
+  if(inputEl.indexOf('=') === 31 && inputEl.length === 43){
+    show.classList.remove("hidden");
+
+   let  uri =  inputEl.split('=')
+
+   const ml = uri[1]
+
+  
+   
+ let watchUrl = `https://i.ytimg.com/vi/${ml}/maxresdefault.jpg`;
+
+
+
+  imageEl.src = watchUrl;
+  imageEl.style.width = `1280px`;
+  imageEl.style.height = `720px`;
+
+
+  }
+   else if(inputEl.indexOf('=') === 31 && inputEl.length === 48){
+
+show.classList.remove("hidden");
 
   const result = inputEl.slice(17, 28);
 
+ 
   let url = `https://i.ytimg.com/vi/${result}/maxresdefault.jpg`;
 
   imageEl.src = url;
 
   imageEl.style.width = `1280px`;
   imageEl.style.height = `720px`;
+   }
+  
+   else{
+
+    show.classList.remove("hidden");
+
+  const result = inputEl.slice(32, 43);
+
+ 
+  let url = `https://i.ytimg.com/vi/${result}/maxresdefault.jpg`;
+
+  imageEl.src = url;
+
+  imageEl.style.width = `1280px`;
+  imageEl.style.height = `720px`;
+   }
+   
 }
 
 
